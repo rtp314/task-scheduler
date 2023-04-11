694eae6c3cf9a1a4b330f803bef21447f8c6ae0d
Samuel Reed <samuelreed01@gmail.com>
2023-04-11 18:12:48 +0700

    add Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Calendar from './components/Calendar';
 import TaskContextProvider from './context/TaskContext';
+import NewTask from './context/NewTask';
+import Sidebar from './components/Sidebar';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,7 +10,15 @@ function App() {
   return (
     <div className="App">
       <TaskContextProvider>
-        <Calendar />
+        <div className="responsive-content-wrapper">
+          <div className="responsive-content-main">
+            <Calendar />
+          </div>
+          <div className="responsive-content-sidebar">
+            <Sidebar />
+          </div>
+        </div>
+        <NewTask />
       </TaskContextProvider>
     </div>
   );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,8 @@
+export default function Sidebar() {
+  return (
+    <div id="sidebar">
+      <div>Currently Selected Task</div>
+      <div>Currently Selected Day</div>
+    </div>
+  );
+}
